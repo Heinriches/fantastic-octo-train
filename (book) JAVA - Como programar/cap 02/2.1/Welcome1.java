c5e3ec6467e78ed3e231542c7dacbad622c88233
Heinriches <heydrich.institut@gmail.com>
2025-11-27 19:10:19 -0300

    Update Welcome1.java

--- (book) JAVA - Como programar/cap 02/2.1/Welcome1.java
+++ (book) JAVA - Como programar/cap 02/2.1/Welcome1.java
@@ -1,15 +1,14 @@
-import java. util.*;
 
 // Figura 2.1: Welcome1.java
 // Programa de impressão de texto.
 
-public class Welcome1{
-	// Método "main" inicia a execução do aplicativo JavaCodebaseComponent
+
+import java. util.*;
+
+public class Welcome1{											// Início da classe "Welcome1".
 	
-	public static void main(String[] args){
-		System.out.println("Welcome to Java Programming!");
-	}
+	public static void main(String[] args){							// Início do método "main" inicia a execução do aplicativo.
+		System.out.println("Welcome to Java Programming!");		
+	}																// fim do método "main".
 	
-	// fim do método "main"
-}
-// fim da classe "Welcome1
+}																// fim da classe "Welcome1".
