8d4050ca1d5ae1bf608e828a36e53c3f775f656a
Heinriches <heydrich.institut@gmail.com>
2025-11-27 18:58:51 -0300

    Create Welcome1.java

---
+++ (book) JAVA - Como programar/cap 02/2.1/Welcome1.java
@@ -0,0 +1,15 @@
+import java. util.*;
+
+// Figura 2.1: Welcome.java
+// Programa de impressão de texto.
+
+public class Welcome1{
+	// Método "main" inicia a execução do aplicativo JavaCodebaseComponent
+	
+	public static void main(Sting[] args){
+		System.out.println("Welcome to Java Programming!");
+	}
+	
+	// fim do método "main"
+}
+// fim da classe "Welcome1
